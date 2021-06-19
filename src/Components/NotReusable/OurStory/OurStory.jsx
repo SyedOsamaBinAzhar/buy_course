042ab9caf9ce753fbc7342d4269fcb17121a9da5
Osama Azhar <osamaazhar.edu@gmail.com>
2021-06-19 14:16:21 +0500

    Heading Component made dynamic

--- src/Components/NotReusable/OurStory/OurStory.jsx
+++ src/Components/NotReusable/OurStory/OurStory.jsx
@@ -8,7 +8,7 @@ import Heading from "../../Reusable/Heading/Heading"
 const OurStory = () => {
     return (
         <div className="ourStoryStyling flex flex-col">
-            <Heading/>
+            <Heading text="Our Story"/>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis asperiores voluptas repudiandae delectus eveniet doloremque alias quisquam libero cupiditate consectetur minima dolores provident sed eligendi suscipit, veritatis numquam mollitia laboriosam?
             Iure odio odit illo corporis dignissimos, quidem saepe. Consequatur eius voluptatum autem illo maiores porro hic est possimus, minima voluptas, doloribus sed ipsa id, delectus ab molestias optio? Excepturi, harum.</p>
         </div>
--- src/Components/Reusable/Heading/Heading.jsx
+++ src/Components/Reusable/Heading/Heading.jsx
@@ -1,12 +1,21 @@
 //importing libraries
 import React from 'react'
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 
 //importing files
 import "./Heading.css"
 
-const Heading = () => {
-    var [heading,setHeading] = useState("Our Story")
+const Heading = ({text}) => {
+    // console.log(text)
+    var [heading,setHeading] = useState("")
+
+    useEffect(() => {
+        setHeading(text)
+        return () => {
+            
+        }
+    }, [])
+
     return (
         <div>
             <h1>{heading}</h1>
